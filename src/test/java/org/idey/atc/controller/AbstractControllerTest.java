12a0d08c96b23f836c60fda6d0625b76e54ed111
INDRANIL DEY <idey@apple.com>
2016-02-28 10:23:14 -0800

    Perform cleanup method for registry testing

--- src/test/java/org/idey/atc/controller/AbstractControllerTest.java
+++ src/test/java/org/idey/atc/controller/AbstractControllerTest.java
@@ -1,6 +1,7 @@
 package org.idey.atc.controller;
 
 import org.idey.atc.registry.AirportRegistry;
+import org.junit.After;
 import org.junit.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +12,13 @@ public abstract class AbstractControllerTest {
     protected static final Logger LOGGER = LoggerFactory.getLogger(AbstractControllerTest.class);
     @Before
     public void setUp() throws NoSuchFieldException, IllegalAccessException {
+        Field instance = AirportRegistry.class.getDeclaredField("INSTANCE");
+        instance.setAccessible(true);
+        instance.set(null, null);
+    }
+
+    @After
+    public void cleanup() throws NoSuchFieldException, IllegalAccessException {
         Field instance = AirportRegistry.class.getDeclaredField("INSTANCE");
         instance.setAccessible(true);
         instance.set(null, null);
